75d49ce7beddb618f0a65d990212fcf3d7eb173e
RaghuKA <arkumar38@outlook.com>
2020-05-14 10:57:34 +0200

    Improved Gui and renamed Gui items

--- QtPyYTdl.py
+++ QtPyYTdl.py
@@ -3,8 +3,8 @@ import youtube_dl
 import os
 from PyQt5 import QtWidgets, QtCore, QtGui, uic
 import sys
-from PyQt5.QtCore import QUrl,QObject,pyqtSlot #, QStringList
-from PyQt5.QtWidgets import QApplication, QFileDialog, QPushButton, QVBoxLayout #, QWidget
+from PyQt5.QtCore import QUrl,QObject,pyqtSlot 
+from PyQt5.QtWidgets import QApplication, QFileDialog, QPushButton, QVBoxLayout 
 import functools 
 import operator
 
@@ -12,43 +12,43 @@ glob_ui_file = "yTGui.ui"
 Ui_MainWindow, QtBaseClass = uic.loadUiType(glob_ui_file)
 
 class mywindow(QtWidgets.QMainWindow, Ui_MainWindow):
-    def __init__(self, download_inputFile_location_string="", download_destination_folder=""):   
+    def __init__(self, dloadIpFile="", dloadDestDir=""):   
         QtWidgets.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        self.comboBox_VideoQuality.addItem("First item") #add item
+        self.comboBox_VideoQuality.addItem("Second item")
+        self.dloadIpFile = dloadIpFile
+        self.dloadDestDir = dloadDestDir
         
-        self.ui = Ui_MainWindow()        
-        self.ui.setupUi(self)
-        self.ui.comboBox_VideoQuality.addItem("First item") #add item
-        self.ui.comboBox_VideoQuality.addItem("Second item")
-        
-        self.download_inputFile_location_string = download_inputFile_location_string
-        self.download_destination_folder = download_destination_folder
-        self.ui.pushButton_Browse_for_InputFile.clicked.connect(self.openFileNameDialog)
-     
-        self.ui.pushButton_Browse_for_DownloadFolder.clicked.connect(self.openFileNameDialog2)
-        self.ui.pushButton_DownloadVideos.clicked.connect(self.DownloadVideos)
+        self.BrowseInputButton.clicked.connect(self.openFileNameDialog)
+        self.BrowseDloaddirButton.clicked.connect(self.openFileNameDialog2)
+        self.DloadVidsButton.clicked.connect(self.DownloadVideos)
 
     def openFileNameDialog(self):
         fileName, _ = QtWidgets.QFileDialog.getOpenFileName(None, "Browse Input File", "", "Input Files (*.txt *.csv) ")
         print(fileName)
-        self.download_inputFile_location_string = fileName
+        self.FilePathField.setText (fileName)
+        self.dloadIpFile = fileName
      
     def openFileNameDialog2(self):
-        DownloadFolderName = str(QtWidgets.QFileDialog.getExistingDirectory(self, 'Select directory'))
-        self.download_destination_folder = DownloadFolderName
+        DloadDir = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select directory')
+        print(DloadDir)
+        self.DloadDirPath.setText (DloadDir)
+        self.dloadDestDir = DloadDir
  
     def DownloadVideos(self):
-        with open(self.download_inputFile_location_string) as f:
+        with open(self.dloadIpFile) as f:
             my_list = list(f)
         print(my_list)
         ydl_opts = {}
        
-        os.chdir(self.download_destination_folder)
+        os.chdir(self.dloadDestDir)
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download(my_list)        
 
-
-app = QtWidgets.QApplication([])
-application = mywindow()
-application.show()
-sys.exit(app.exec())
+if __name__ == "__main__":
+    app = QtWidgets.QApplication (sys.argv)
+    window = mywindow ()
+    window.show ()
+    sys.exit (app.exec_())
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # PyQtYouTube-dl
 Gui for downloading youtube videos
 
+*	Takes input links from a .txt or .csv file
+*	Custom selection option for download directory
+
+
